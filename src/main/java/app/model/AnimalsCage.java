f606502a1af16495e9f7f9995800946fd126f952
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-16 18:01:52 +0300

    This is the solution to task 2.1.3
    Pre-project

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,29 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
+    @Autowired                // ТЗ: свяжите AnimalCage c бином Dog через абстрактный класс Animal
+    @Qualifier("dog")         // явно указываем, какой бин нужно внедрить, т.к. подходящих бинов два: cat и dog
     private Animal animal;
+
+
+    @Autowired
+    private Timer timer;      // ТЗ: на основе класса Timer создайте бин, свяжите с AnimalCage
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
